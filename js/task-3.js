84aa8921cd4bc50c57db638201ecb02dd4aed869
Cassel444 <150070945+Cassel444@users.noreply.github.com>
2024-02-02 15:39:23 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function getElementWidth(content, padding, border) {
+    const numContent = Number.parseFloat(content);
+    const numPadding = Number.parseFloat(padding);
+    const numBorder = Number.parseFloat(border);
+    const elementWidth = numContent + numPadding * 2 + numBorder * 2;
+    return elementWidth;
+
+}
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
+
+
+
+
+
+
