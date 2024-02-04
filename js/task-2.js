307b60c8e02e1399973cb3188cd018446eab71cb
Cassel444 <chumachocl@gmail.com>
2024-02-04 14:52:23 +0200

    Made tutor's recommendations HW 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,8 @@
-// function getShippingMessage(country, price, deliveryFee) {
-//     const totalPrice = price + deliveryFee;
-//     return `Shipping to ${country} will cost ${totalPrice} credits`;
-// }
-// console.log(getShippingMessage("Australia", 120, 50));
-// console.log(getShippingMessage("Germany", 80, 20));
-// console.log(getShippingMessage("Sweden", 100, 20));
+
 
 function formatMessage(message, maxLength) {
     if (message.length <= maxLength) {
-        return message.slice();
+        return message;
     }
     else {
         return message.slice(0, maxLength) + "...";
--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,13 @@
-// function getElementWidth(content, padding, border) {
-//     const numContent = Number.parseFloat(content);
-//     const numPadding = Number.parseFloat(padding);
-//     const numBorder = Number.parseFloat(border);
-//     const elementWidth = numContent + numPadding * 2 + numBorder * 2;
-//     return elementWidth;
-
-// }
-// console.log(getElementWidth("50px", "8px", "4px"));
-// console.log(getElementWidth("60px", "12px", "8.5px"));
-// console.log(getElementWidth("200px", "0px", "0px"));
 
 function checkForSpam(message) {
-    const newMessage = message.toLowerCase();
-    const checkWord = "spam";
-    const checkWord1 = "sale";
-    if (newMessage.includes(checkWord) || newMessage.includes(checkWord1)) {
+    const normalizedMessage = message.toLowerCase();
+    if (normalizedMessage.includes("spam") || normalizedMessage.includes("sale")) {
         return true;
     } else {
         return false;
     }
 
 }
-
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
@@ -29,6 +15,4 @@ console.log(checkForSpam("Amazing SalE, only tonight!")); // true
 console.log(checkForSpam("Trust me, this is not a spam message")); // true
 console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
 console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-
-
 
