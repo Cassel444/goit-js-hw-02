77418ce64cae5c8dfe1c84a087f2da598f44ee0d
Cassel444 <chumachocl@gmail.com>
2024-02-09 12:08:08 +0200

    Update hw-3

--- js/task-3.js
+++ js/task-3.js
@@ -6,7 +6,6 @@ function checkForSpam(message) {
     } else {
         return false;
     }
-
 }
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
